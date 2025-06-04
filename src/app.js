df904663d85b7d474ce9441ff1e7707212fef557
Eula Daniela Yamil <euladanielayamil@gmail.com>
2025-06-04 16:39:04 +0000

    Eula-CartasAleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(cartasAleatorias) {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let palos = ["♥","♠","♣","♦"];
+let cartaValor = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+
+let randomPalos = palos [Math.floor(Math.random()* palos.length)];
+let randomValor = cartaValor [Math.floor(Math.random()* cartaValor.length)];
+console.log(randomPalos);
+console.log(randomValor);
+// si es corazon o diamante sera rojo sino negro
+
+if (randomPalos === "♥" || randomPalos === "♦") {
+  document.querySelector(".top").innerHTML = randomPalos;
+    document.querySelector(".bottom").innerHTML = randomPalos;
+ 
+document.querySelector(".top").style.color="red";
+document.querySelector(".bottom").style.color="red";
+ } else {
+document.querySelector(".bottom").style.color = "black";
+document.querySelector(".top").style.color = "black";
+  document.querySelector(".top").innerHTML = randomPalos;
+    document.querySelector(".bottom").innerHTML = randomPalos;
+  
+ }
+
+document.querySelector(".number").innerHTML = randomValor
 };
