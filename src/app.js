127b9ea3e46aa5ccf182c62fbbb36f539748d72f
Eula Daniela Yamil <euladanielayamil@gmail.com>
2025-06-16 01:05:23 +0000

    giré el palo de abajo con tranform:rotate 180deg, anexé botn con conteo regresivo y otro para cambiar la carta

--- src/app.js
+++ src/app.js
@@ -5,30 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(cartasAleatorias) {
-  //write your code here
-let palos = ["♥","♠","♣","♦"];
-let cartaValor = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
 
-let randomPalos = palos [Math.floor(Math.random()* palos.length)];
-let randomValor = cartaValor [Math.floor(Math.random()* cartaValor.length)];
-console.log(randomPalos);
-console.log(randomValor);
-// si es corazon o diamante sera rojo sino negro
+window.onload = function() {
+  let palos = ["♥", "♠", "♣", "♦"];
+  let cartaValor = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
-if (randomPalos === "♥" || randomPalos === "♦") {
-  document.querySelector(".top").innerHTML = randomPalos;
+  let tiempo = 10;
+  let intervalo;
+
+
+  function generarCarta() {
+    let randomPalos = palos[Math.floor(Math.random() * palos.length)];
+    let randomValor = cartaValor[Math.floor(Math.random() * cartaValor.length)];
+    //utilizo mejor condicional ternario :)
+    let color = randomPalos === "♥" || randomPalos === "♦" ? "red" : "black";
+
+    document.querySelector(".top").innerHTML = randomPalos;
     document.querySelector(".bottom").innerHTML = randomPalos;
- 
-document.querySelector(".top").style.color="red";
-document.querySelector(".bottom").style.color="red";
- } else {
-document.querySelector(".bottom").style.color = "black";
-document.querySelector(".top").style.color = "black";
-  document.querySelector(".top").innerHTML = randomPalos;
-    document.querySelector(".bottom").innerHTML = randomPalos;
-  
- }
+    document.querySelector(".top").style.color = color;
+    document.querySelector(".bottom").style.color = color;
+    document.querySelector(".number").innerHTML = randomValor;
 
-document.querySelector(".number").innerHTML = randomValor
+
+  }
+  function inicioConteo() {
+    tiempo = 10; 
+    intervalo = setInterval(() => {
+      tiempo--;
+      document.getElementById("botonConteo").innerHTML = `Cambiala en ${tiempo}s`;
+      if (tiempo === 0){
+        clearInterval(intervalo);
+        generarCarta();
+          document.getElementById("botonConteo").innerHTML = "Cámbiala en 10s";
+      }
+      
+    }, 1000);
+    
+    
+  }
+  document.getElementById("botonConteo").addEventListener("click", inicioConteo);
+
+  generarCarta();
+
+  document.getElementById("boton").addEventListener("click", generarCarta);
 };
